7d7c8bb67b2ad6e85ff1fb6be38cfea6c589ee0a
FormaktProject <formaktproject@gmail.com>
2026-08-05 21:47:23 +0100

    fix build error

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9oIMKIp15wvJOXBJrQrzZMn5ujWc7m0c+stMwdVXMfU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
